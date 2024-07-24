dc9af6d59045798d8fcfb1d26f38269f4c34a7d1
Nikolas Savolainen <nikolassavolainen@gmail.com>
2024-07-24 14:08:07 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -52,11 +52,16 @@ class Dev(Configuration):
         'django.contrib.contenttypes',
         'django.contrib.sessions',
         'django.contrib.messages',
+        "django.contrib.sites",
         'django.contrib.staticfiles',
         'blog',
         "crispy_forms",
         "crispy_bootstrap5",
         "debug_toolbar",
+        "allauth",
+        "allauth.account",
+        "allauth.socialaccount",
+        "allauth.socialaccount.providers.google",
     ]
 
     MIDDLEWARE = [
@@ -70,6 +75,13 @@ class Dev(Configuration):
         #'django.middleware.clickjacking.XFrameOptionsMiddleware',
     ]
 
+    SITE_ID = 1
+
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_AUTHENTICATION_METHOD = "email"
+    
     ROOT_URLCONF = 'blango.urls'
 
     INTERNAL_IPS = ["192.168.11.51"]
