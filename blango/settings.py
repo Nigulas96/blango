a168e686640f2e9cf3227015efbc1ba764ba9172
Nikolas Savolainen <nikolassavolainen@gmail.com>
2024-08-06 11:27:37 +0000

    Finish authentication

--- blango/settings.py
+++ blango/settings.py
@@ -38,6 +38,14 @@ class Dev(Configuration):
     CSRF_COOKIE_SAMESITE = 'None'
     SESSION_COOKIE_SAMESITE = 'None'
     
+    REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ]
+}
+
 
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
@@ -63,6 +71,7 @@ class Dev(Configuration):
         "allauth.socialaccount",
         "allauth.socialaccount.providers.google",
         "rest_framework",
+        "rest_framework.authtoken",
     ]
 
     MIDDLEWARE = [
