adf87fbf9a370867c1243f05f2c86fd83fb5a63c
Nikolas Savolainen <nikolassavolainen@gmail.com>
2024-08-17 15:12:05 +0000

    Finish testing DRF with mocks

--- blango_auth/models.py
+++ blango_auth/models.py
@@ -1,16 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, UserManager
 from django.utils.translation import gettext_lazy as _
-
 # Create your models here.
 
-
 class BlangoUserManager(UserManager):
-
     def _create_user(self, email, password, **extra_fields):
-
         if not email:
-            raise ValueError('Emaile must be set')
+            raise ValueError("Email must be set")
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
@@ -18,21 +14,20 @@ class BlangoUserManager(UserManager):
         return user
 
     def create_user(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', False)
-        extra_fields.setdefault('is_superuser', False)
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
         return self._create_user(email, password, **extra_fields)
 
-    def create_superuser(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
 
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
 
-        return self._create_user( email, password, **extra_fields)
-
+        return self._create_user(email, password, **extra_fields)
 
 class User(AbstractUser):
     username = None
@@ -41,10 +36,10 @@ class User(AbstractUser):
         unique=True,
     )
 
-    objects: BlangoUserManager()
+    objects = BlangoUserManager()
 
-    USERNAME_FIELD= "email"
-    REQUIRED_FIELDS= []
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
 
     def __str__(self):
         return self.email
