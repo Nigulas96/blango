e4a0c2965dd2957c2dc57e40d0b6844face3f27c
Nikolas Savolainen <nikolassavolainen@gmail.com>
2024-09-03 17:42:17 +0000

    Finish simplejwt

--- blango/settings.py
+++ blango/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from configurations import Configuration
 from configurations import values
 import dj_database_url
+from datetime import timedelta
 
 class Dev(Configuration):
     # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,12 +38,18 @@ class Dev(Configuration):
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SAMESITE = 'None'
     SESSION_COOKIE_SAMESITE = 'None'
+
+    SIMPLE_JWT = {
+        "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+        "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    }
     
     REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication"
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticatedOrReadOnly"
@@ -65,6 +72,7 @@ class Dev(Configuration):
             "django_filters.rest_framework.DjangoFilterBackend",
             "rest_framework.filters.OrderingFilter"
         ],
+
     
 }
 
